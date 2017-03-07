92fdb901da2c4b998e8e96606b979e7c70121203
Falaleev Maxim <max@studio107.ru>
2017-03-07 16:23:59 +0300

    Fix tests

--- Tests/DummyEntity.php
+++ Tests/DummyEntity.php
@@ -4,8 +4,6 @@
  * (c) Studio107 <mail@studio107.ru> http://studio107.ru
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
- *
- * Author: Maxim Falaleev <max@studio107.ru>
  */
 
 namespace Orion\Component\Client\Tests;
@@ -55,20 +53,5 @@ class DummyEntity extends AbstractApiEntity
     public function setName($name)
     {
         $this->name = $name;
-    }
-}
-
-class EntityTest extends \Framework_TestCaseTest
-{
-    public function testToArray()
-    {
-        $entity = new DummyEntity();
-        $entity->setName('foo');
-        $entity->setPrice(321);
-
-        $this->assertSame([
-            'name' => 'foo',
-            'price' => 321
-        ], $entity->toArray());
     }
 }
---
+++ Tests/EntityTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * (c) Studio107 <mail@studio107.ru> http://studio107.ru
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * Author: Maxim Falaleev <max@studio107.ru>
+ */
+
+namespace Orion\Component\Client\Tests;
+
+class EntityTest extends \Framework_TestCaseTest
+{
+    public function testToArray()
+    {
+        $entity = new DummyEntity();
+        $entity->setName('foo');
+        $entity->setPrice(321);
+
+        $this->assertSame([
+            'name' => 'foo',
+            'price' => 321
+        ], $entity->toArray());
+    }
+}
